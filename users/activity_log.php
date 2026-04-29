78b6ad1191e9bf1ca23b5cf89cf587f5c43fbd6d
Lidetu <lidosinafanis@gmail.com>
2026-04-30 02:29:10 +0300

    activity for users log

---
+++ users/activity_log.php
@@ -0,0 +1,11 @@
+<?php
+// activity_log.php - User Activity Log
+include_once '../functions.php';
+// Example: Display user activity log (pseudo code)
+// $logs = get_user_activity_logs();
+$logs = [
+    ['user' => 'alice', 'action' => 'login', 'time' => '2026-04-30 10:00'],
+    ['user' => 'bob', 'action' => 'logout', 'time' => '2026-04-30 10:05'],
+];
+?>
+
